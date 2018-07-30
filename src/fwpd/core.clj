fd42f0ab59487fabf67542641e8c25b366c98954
Mark Valdez <mark.valdez@rmn.com>
2018-07-30 13:28:50 -0500

    protocols

--- src/fwpd/core.clj
+++ src/fwpd/core.clj
@@ -1,33 +1,39 @@
 (ns fwpd.core)
-(def filename "suspects.csv")
 
-(def vamp-keys [:name :glitter-index])
+(defprotocol Foo
+  (bar [x]))
 
-(defn str->int
-  "Function to convert a string to an integer"
-  [str]
-  (Integer. str))
+(extend-type java.lang.String
+  Foo
+  (bar [x] (str "hello, " x)))
 
-(def conversions {:name identity
-                  :glitter-index str->int})
+(bar (str "world"))
 
-(defn convert
-  "Convert values based on conversions"
-  [vamp-key value]
-  ((get conversions vamp-key) value))
+(comment (bar {}))
 
-(defn parse
-  "Convert a CSV into rows of columns"
-  [string]
-  (map #(clojure.string/split % #",")
-       (clojure.string/split string #"\n")))
+(def my-map {:a 1, :b 2})
+(println (my-map :a))
+(println (:a my-map))
+(remove nil? [nil 1 2 3])
+(println ((fnil int 1) nil))
 
-(defn mapify
-  "Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10}"
-  [rows]
-  (map (fn [unmapped-row]
-         (reduce (fn [row-map [vamp-key value]]
-                   (assoc row-map vamp-key (convert vamp-key value)))
-                 {}
-                 (map vector vamp-keys unmapped-row)))
-       rows))
+;; protocol + records
+(defprotocol Human
+  (say-hello [x]))
+
+(defrecord American [first-name last-name]
+  Human
+  (say-hello [x]
+    (str "hello, my name is " first-name " " last-name)))
+
+(defrecord Mexican [first-name last-name]
+  Human
+  (say-hello [x]
+    (str "hola, me llamo " first-name " " last-name)))
+
+(println (say-hello (map->American {:first-name "Joe" :last-name "Smith" :city "Austin"})))
+(println (say-hello (map->Mexican {:first-name "Jose" :last-name "Martinez" :city "Cancun"})))
+
+;; alternate ways of instantiating the record
+(comment (println (say-hello (Mexican.  "Jose"  "Martinez" "Cancun")))) ;; fails with no matching ctor
+(comment (println (say-hello (->Mexican  "Jose"  "Martinez" "Cancun")))) ;; fails wrong number of args
--- test/fwpd/core_test.clj
+++ test/fwpd/core_test.clj
@@ -1,7 +1,11 @@
 (ns fwpd.core-test
   (:require [clojure.test :refer :all]
-            [fwpd.core :refer :all]))
+            [clojure.test.check :as tc]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def sort-idempotent-prop
+  (prop/for-all [v (gen/vector gen/int)]
+                (= (sort v) (sort (sort v)))))
+
+(tc/quick-check 100 sort-idempotent-prop)
